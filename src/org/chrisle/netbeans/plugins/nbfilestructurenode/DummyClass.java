68a5efacc67979337677d62e0be5a0319d7df085
Chris2011 <Christian.Lenz@gmx.net>
2016-01-28 21:13:24 +0100

    Added new, better icons.

---
+++ src/org/chrisle/netbeans/plugins/nbfilestructurenode/DummyClass.java
@@ -0,0 +1,29 @@
+package org.chrisle.netbeans.plugins.nbfilestructurenode;
+
+/**
+ *
+ * @author ChrisLE
+ */
+public class DummyClass {
+    
+}
+
+abstract class test {
+    
+}
+
+interface tester2 {
+    
+}
+
+@interface tester {
+    
+}
+
+enum blau {
+    
+}
+
+class TException extends Exception {
+    
+}
