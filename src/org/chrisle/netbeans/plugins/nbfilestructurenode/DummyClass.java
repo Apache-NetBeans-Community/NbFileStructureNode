642e217f073cb430fef5ce36bb78104e97cd6fa7
Chris <chrl@spreadshirt.net>
2016-02-09 17:11:30 +0100

    Added methods and classes for dummy class.

--- src/org/chrisle/netbeans/plugins/nbfilestructurenode/DummyClass.java
+++ src/org/chrisle/netbeans/plugins/nbfilestructurenode/DummyClass.java
@@ -5,7 +5,11 @@ package org.chrisle.netbeans.plugins.nbfilestructurenode;
  * @author ChrisLE
  */
 public class DummyClass {
+    private String _testInDummyClass;
     
+    protected String blaInDummyClass() {
+        return "";
+    }
 }
 
 abstract class test {
@@ -13,11 +17,13 @@ abstract class test {
 }
 
 final class foo {
-    
+    public final static void inFooClass() {
+        
+    }
 }
 
 interface tester2 {
-    
+    public void inTester2Interface();
 }
 
 @interface tester {
@@ -30,4 +36,18 @@ enum blau {
 
 class TException extends Exception {
     
+}
+
+class bar {
+    class foo {
+        class barbar {
+            class barbarfoo {
+                
+            }
+        }
+    }
+    
+    class foot {
+        
+    }
 }
